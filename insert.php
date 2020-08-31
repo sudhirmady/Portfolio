09bfce19af55edbb0f4de9aeba9a11e53f4b89da
Sudhir Mady <sudheer.madisetty111@gmail.com>
2020-08-31 21:16:42 +0530

    'Mail2'

--- insert.php
+++ insert.php
@@ -7,13 +7,10 @@
 
         $to='sudheer.madisetty111@gmail.com'; 
         $subject='Sudhir Portfolio';
-        $message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+        $message="Name :" .$name. "\n". "Phone :" .$phone. "\n" . "Wrote the following :" . "\n\n" . $msg;
         $headers="From: ".$email;
 
         mail($to, $subject, $message, $headers);
-         
-        header('Location: index.php');
-        echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-        
+        header('Location: index.php?mailsend');        
     }
 ?>
